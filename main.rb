b37816e073f4a59d5da54b0127ac53752c1bd294
Brian Hodges <bhodges@PMIDSD.com>
2017-08-07 14:23:13 -0400

    simplify code more

--- main.rb
+++ main.rb
@@ -1,23 +1,21 @@
-dir = ARGV[0].strip.to_s unless !ARGV[0]
-search = ARGV[1].strip.to_s unless !ARGV[1]
+puts
+if ARGV.size != 2
+	puts 'You must enter both parameters.'
+	puts 'Ex. ruby main.rb C:\path\to\directory "pattern match"'
+	exit
+end
+
+dir = ARGV[0].strip.to_s
+search = ARGV[1].strip.to_s
 matches = []
 
-puts
-puts
-if dir && search
-	Dir.foreach(dir) do |file|
-		if file.length > 3
-			path = dir + '\\' + file
-			text = File.open(path).read
-			text.each_line do |line|
-				matches << path if line.include? search
-			end
-		end
+Dir.foreach(dir) do |file|
+	if file.length > 3
+		path = dir + '\\' + file
+		text = File.open(path).read
+		text.each_line{ |line| matches << path if line.include? search }
 	end
-	puts 'Results:'
-	puts '========'
-	puts !matches.empty? ? matches.each{|filename| filename } : 'No matches found.'
-else
-	puts 'You must enter both parameters.'
-	puts 'Ex. ruby grep.rb C:\path\to\directory "pattern match"'
 end
+puts 'Results:'
+puts '========'
+puts !matches.empty? ? matches.each{|filename| filename } : 'No matches found.'
